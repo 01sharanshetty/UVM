8c5f3c8970c51a6a922c0bee79d74937706e1a37
01sharanshetty <126483491+01sharanshetty@users.noreply.github.com>
2024-07-05 12:55:47 +0530

    Update xgemac_driver.sv

--- xgemac_driver.sv
+++ xgemac_driver.sv
@@ -45,12 +45,13 @@ virtual task run_phase(uvm_phase phase);
           begin
           isi.pkt_tx_val<=1;
           isi.pkt_tx_eop<=0;
+          if(pkt_count>0)
+              isi.pkt_tx_sop<=1;
+          else
+              isi.pkt_tx_sop<=0;
           isi.pkt_tx_data <= isi.frame.pop_back();
           pkt_count<=pkt_count+1;
-              if(pkt_count>0)
-                  isi.pkt_tx_sop<=1;
-              else
-                  isi.pkt_tx_sop<=0;
+
           end
         else 
           begin
